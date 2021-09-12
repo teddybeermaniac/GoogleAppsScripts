803e640f4b0f9d025b63b00c63f4233e4c6b77fd
Michał Przybyś <michal@przybys.eu>
2021-09-12 14:18:53 +0200

    Add INTO function support (#21)
    * Disable caching of queries which return null or undefined.

--- helpers-querying/src/providers/base-alasql-queryable-provider/base-alasql-queryable-provider.ts
+++ helpers-querying/src/providers/base-alasql-queryable-provider/base-alasql-queryable-provider.ts
@@ -25,10 +25,9 @@ import alasql from 'alasql';
 import type { ICache } from 'helpers-caching';
 import type { ILogger } from 'helpers-logging';
 import { injectable, interfaces } from 'inversify';
-import objectHash from 'object-hash';
 
 import { fromMethodsSymbol, IAlaSQLFunctionSymbol, intoMethodsSymbol } from '../../symbols';
-import type { IQueryableProvider } from '../iqueryable-provider';
+import { BaseQueryableProvider } from '../base-queryable-provider';
 import type { ProviderType } from '../provider-type';
 import type { IFromMethod } from './from-method/ifrom-method';
 import type { IFromMethodOptions } from './from-method/ifrom-method-options';
@@ -37,7 +36,7 @@ import type { IIntoMethod } from './into-method/iinto-method';
 import type { IIntoMethodOptions } from './into-method/iinto-method-options';
 
 @injectable()
-export abstract class BaseAlaSQLQueryableProvider implements IQueryableProvider {
+export abstract class BaseAlaSQLQueryableProvider extends BaseQueryableProvider {
   private static readonly defaultFromMethodOptions: IFromMethodOptions = {};
 
   private static readonly defaultIntoMethodOptions: IIntoMethodOptions = {
@@ -48,26 +47,15 @@ export abstract class BaseAlaSQLQueryableProvider implements IQueryableProvider 
 
   public abstract get providerType(): ProviderType;
 
-  constructor(protected readonly logger: ILogger, private readonly cache: ICache,
-    private container: interfaces.Container) {
+  constructor(logger: ILogger, cache: ICache, private container: interfaces.Container) {
+    super(logger, cache);
+
     if (!BaseAlaSQLQueryableProvider._alasqlInitialized) {
       this.addFromMethods();
       this.addIntoMethods();
       this.addFunctions();
       BaseAlaSQLQueryableProvider._alasqlInitialized = true;
     }
-  }
-
-  private getCache<TValue>(query: string, cacheKey: string | boolean, parameters: any[]):
-  any | null {
-    this.logger.debug(`Getting result of '${query}' query from cache`);
-    return this.cache.get<TValue>(objectHash.sha1([query, cacheKey, parameters]));
-  }
-
-  private putCache<TValue>(query: string, cacheKey: string | boolean, parameters: any[],
-    result: any) {
-    this.logger.debug(`Putting result of '${query}' query into cache`);
-    this.cache.set<TValue>(objectHash.sha1([query, cacheKey, parameters]), result);
   }
 
   private addFromMethods(): void {
@@ -124,44 +112,11 @@ export abstract class BaseAlaSQLQueryableProvider implements IQueryableProvider 
     });
   }
 
-  public query<TRow>(query: string, cacheKey: string | boolean | null, ...parameters: any[]):
-  TRow[] {
-    this.logger.debug(`Querying a model ${cacheKey ? 'with cache' : 'without cache'} with query '${query}'`);
-    if (cacheKey) {
-      const cached = this.getCache<TRow[]>(query, cacheKey, parameters);
-      if (cached !== null) {
-        this.logger.debug('Result found in cache');
-        return cached;
-      }
-    }
-
-    this.logger.debug('Running query');
-    const result = (<any[]>alasql(query, parameters)).map((row) => <TRow>row);
-    if (cacheKey) {
-      this.putCache<TRow[]>(query, cacheKey, parameters, result);
-    }
-
-    this.logger.trace(`The result of the query is '${JSON.stringify(result)}'`);
-    return result;
+  protected runQuery<TRow>(query: string, parameters: any[]): TRow[] {
+    return (<any[]>alasql(query, parameters)).map((row) => <TRow>row);
   }
 
-  public queryAny(query: string, cacheKey: string | boolean | null, ...parameters: any[]): any[][] {
-    this.logger.debug(`Querying any ${cacheKey ? 'with cache' : 'without cache'} with query '${query}'`);
-    if (cacheKey) {
-      const cached = this.getCache<any[][]>(query, cacheKey, parameters);
-      if (cached !== null) {
-        this.logger.debug('Result found in cache');
-        return cached;
-      }
-    }
-
-    this.logger.debug('Running query');
-    const result = alasql(query, parameters);
-    if (cacheKey) {
-      this.putCache<any[][]>(query, cacheKey, parameters, result);
-    }
-
-    this.logger.trace(`The result of the query is '${JSON.stringify(result)}'`);
-    return result;
+  protected runQueryAny(query: string, parameters: any[]): any[][] {
+    return alasql(query, parameters);
   }
 }
